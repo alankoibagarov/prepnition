b48632dec0fcf8e984fc3a6f07815f595f5d3fce
alan <akoibagaroff@gmail.com>
2026-06-19 17:49:36 +0500

    Extended Access Token age

--- lib/auth/constants.ts
+++ lib/auth/constants.ts
@@ -4,7 +4,7 @@ export const REFRESH_TOKEN_COOKIE = "refresh_token";
 export const ACCESS_TOKEN_TTL = "15m";
 export const REFRESH_TOKEN_TTL = "7d";
 
-export const ACCESS_TOKEN_MAX_AGE = 15 * 60;
+export const ACCESS_TOKEN_MAX_AGE = 24 * 60 * 60; // 24 hours
 export const REFRESH_TOKEN_MAX_AGE = 7 * 24 * 60 * 60;
 
 export const PROTECTED_PAGE_PREFIXES = ["/app"];
